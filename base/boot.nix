ec99fa3e8c74308d3819624befc861b681132a13
Aniket Deshpande <aniketd@users.noreply.github.com>
2018-12-04 11:55:41 +0000

    Add updated boot options

--- base/boot.nix
+++ base/boot.nix
@@ -4,8 +4,16 @@
   # Use the systemd-boot EFI boot loader.
   boot = {
     loader = {
-      systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        enable = true;
+        editor = false;
+      };
+      grub = {
+        fsIdentifier = "label";
+        memtest86.enable = true;
+        useOSProber = true;                   # Detect other OS's, if installed in other partitions.
+      };
     };
   };
 }
