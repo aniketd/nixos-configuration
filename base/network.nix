19ab9289d9dea9b3c00989d7407bca344fb43bbc
Aniket Deshpande <aniketd@users.noreply.github.com>
2018-12-04 11:00:40 +0000

    network.nix: Add comment about wpa_supplicant

--- base/network.nix
+++ base/network.nix
@@ -1,5 +1,7 @@
 { config, pkgs, ... }:
 
 {
+  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+  # wpa_supplicant is incompatible with networkmanager.
   networking.networkmanager.enable = true;
 }
